38e4896a4420f24d219c08c099d9a209958b9903
wwwtomaselli <w_msg@yahoo.com.br>
2016-12-05 21:55:17 -0200

    LivrariaBundle criada

---
+++ src/LivrariaBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LivrariaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('LivrariaBundle:Default:index.html.twig');
+    }
+}
---
+++ src/LivrariaBundle/Entity/Produtos.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LivrariaBundle\Entity;
+
+/**
+ * Description of Produtos
+ *
+ * @author aluno
+ */
+class Produtos 
+{
+    private $id;
+    private $nome;
+    private $quantidade;
+    private $preco;
+    private $tipo;
+    private $imagem;
+}
